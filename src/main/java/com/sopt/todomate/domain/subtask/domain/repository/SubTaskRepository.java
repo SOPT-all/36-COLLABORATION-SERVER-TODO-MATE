4fabf7b77e3c0fbcb6ad9ffb05bbaa5dcf087836
Jeon Chan Min <chanmin010702@gmail.com>
2025-05-14 16:28:37 +0900

    feat: 루틴 상세 조회를 위한 mainTaskId 기반 subTask 일괄 조회 쿼리 추가

--- src/main/java/com/sopt/todomate/domain/subtask/domain/repository/SubTaskRepository.java
+++ src/main/java/com/sopt/todomate/domain/subtask/domain/repository/SubTaskRepository.java
@@ -3,6 +3,8 @@ package com.sopt.todomate.domain.subtask.domain.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sopt.todomate.domain.maintask.domain.entity.MainTask;
@@ -13,4 +15,10 @@ public interface SubTaskRepository extends JpaRepository<SubTask, Long> {
 	List<SubTask> findAllByMainTask(MainTask mainTask);
 
 	void deleteAllByMainTask(MainTask mainTask);
+
+	@Query("""
+        SELECT s FROM SubTask s
+        WHERE s.mainTask.id IN :mainTaskIds
+    """)
+	List<SubTask> findAllByMainTaskIds(@Param("mainTaskIds") List<Long> mainTaskIds);
 }
