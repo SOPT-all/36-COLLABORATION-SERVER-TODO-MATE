25821d98176baeb396589477bcba9194ae8e968d
rootTiket <l20078829@gmail.com>
2025-05-15 15:41:41 +0900

    test: 메인태스크 개수 제한 테스트

--- src/test/java/com/sopt/todomate/domain/maintask/application/usecase/MainTaskManageUsecaseTest.java
+++ src/test/java/com/sopt/todomate/domain/maintask/application/usecase/MainTaskManageUsecaseTest.java
@@ -3,6 +3,7 @@ package com.sopt.todomate.domain.maintask.application.usecase;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import com.sopt.todomate.domain.maintask.domain.entity.CategoryType;
 import com.sopt.todomate.domain.maintask.domain.entity.MainTask;
 import com.sopt.todomate.domain.maintask.domain.repository.MainTaskRepository;
 import com.sopt.todomate.domain.maintask.domain.service.MainTaskGetService;
+import com.sopt.todomate.domain.maintask.exception.MaxMainTaskException;
 import com.sopt.todomate.domain.maintask.presentation.dto.MainTaskCreateRequest;
 import com.sopt.todomate.domain.maintask.presentation.dto.MainTaskCreateResponse;
 import com.sopt.todomate.domain.subtask.domain.repository.SubTaskRepository;
@@ -72,5 +74,41 @@ public class MainTaskManageUsecaseTest {
 			.isEqualTo(CategoryType.CATEGORY1);
 		assertThat(mainTask.getTaskDate())
 			.isEqualTo(now);
+	}
+
+	@Test
+	@DisplayName("유저는 한 카테고리당 3개 이상의 메인태스크를 생성할 수 없다.")
+	void mainTaskCreateAmountTest() {
+		// Given - 테스트 사용자 생성 및 저장
+		User testUser = User.builder()
+			.userName("통합테스트유저")
+			.build();
+		User savedUser = userRepository.save(testUser);
+
+		LocalDateTime now = LocalDateTime.now();
+
+		MainTaskCreateRequest request = new MainTaskCreateRequest(
+			"통합테스트 태스크",
+			CategoryType.CATEGORY1,
+			now);
+
+		MainTaskCreateRequest request2 = new MainTaskCreateRequest(
+			"통합테스트 태스크",
+			CategoryType.CATEGORY1,
+			now);
+
+		MainTaskCreateRequest request3 = new MainTaskCreateRequest(
+			"통합테스트 태스크",
+			CategoryType.CATEGORY1,
+			now);
+
+		mainTaskRepository.saveAll(
+			List.of(MainTask.createMainTaskWithoutRoutine("content", CategoryType.CATEGORY1, now, savedUser),
+				MainTask.createMainTaskWithoutRoutine("content", CategoryType.CATEGORY1, now, savedUser)));
+
+		// When & Then
+		assertThatThrownBy(() -> mainTaskManageUsecase.createMainTask(MainTaskCommand.from(request3),
+			savedUser.getId())).isInstanceOf(MaxMainTaskException.class);
+
 	}
 }
